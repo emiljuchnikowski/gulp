0672826345f671a05971cc90fd5d0f6fe9a92c57
Emil Juchnikowski <Emil Juchnikowski>
2016-06-06 11:13:40 +0200

    CSS Compilation

---
+++ plurasight/3 - CSS Compilation/gulp.config.js
@@ -0,0 +1,12 @@
+module.exports = function() {
+    var config = {
+        temp: './.tmp',
+        alljs: [
+            './*.js'
+        ],
+        
+        less: [ './styles/styles.less' ]
+    };
+    
+    return config;
+}
---
+++ plurasight/3 - CSS Compilation/gulpfile.js
@@ -0,0 +1,68 @@
+var gulp = require('gulp');
+var args = require('yargs').argv;
+var config = require('./gulp.config')();
+
+var $ = require('gulp-load-plugins')({ lazy: true });
+
+// var jshint = require('gulp-jshint');
+// var jscs = require('gulp-jscs');
+// var util = require('gulp-util');
+// var gulpprint = require('gulp-print');
+// var gulpif = require('gulp-if');
+
+gulp.task('vet', function () {
+    log("Test log.");
+    
+    return gulp
+        .src(config.alljs)
+        .pipe($.gulpif(args.verbose, $.gulpprint()))
+        .pipe($.jscs())
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
+        .pipe($.jshint.reporter('fail'));
+});
+
+gulp.task('styles', [ 'clean-styles' ], function () {
+    log('less');
+    
+    return gulp
+        .src(config.less)
+        .pipe($.plumber())
+        .pipe($.less())
+        //.on('error', errorLogger)
+        .pipe($.autoprefixer({ browsers: [ 'last 2 version', '> 5%' ] }))
+        .pipe(gulp.dest(config.temp));
+});
+
+gulp.task('clean-styles', function (done) {
+   var files = config.tmp + '**/*.css';
+   clean(files, done);
+});
+
+gulp.task('less-watch', function () {
+    gulp.watch([config.less], ['styles']);
+});
+
+function errorLogger(error) {
+    log('*** Start of Error ***');
+    log(error);
+    log('*** End of Error ***');
+    this.emit('end');
+}   
+
+function clean(path, done) {
+    log('Cleaning: ' + $.util.colors.blue(path));
+    del(path, done);
+}
+
+function log(msg) {
+    if (typeof(msg) === 'object') {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                $.util.log($.util.colors.blue(msg[item]));
+            }
+        }
+    } else {
+        $.util.log($.util.colors.blue(msg));
+    }
+}
