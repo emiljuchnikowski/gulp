e83d4166ae9da870a244a85a518cc1e622ad4f63
Emil Juchnikowski <Emil Juchnikowski>
2016-06-27 11:00:41 +0200

    123

---
+++ plurasight/11 - Testing/gulp.config.js
@@ -0,0 +1,43 @@
+module.exports = function() {
+
+    var config = {
+        temp: './.tmp',
+        alljs: [
+            './*.js'
+        ],
+        html: '**/*.html',
+        less: [ './styles/styles.less' ],
+        index: 'index.html',
+        css: 'styles.css',
+        js: [
+            '**/*.module.js',
+            '!**/*.spec.js' // exclude
+        ],
+        client: '',
+        server: './src',
+        bower: {
+            json: require('./bower.json'),
+            directory: './bower_components/',
+            ignorePath: '../..'
+        },
+        browserReloadDelay: 1000,
+        packages: [
+            './package.json',
+            './bower.json'
+        ],
+        defaultPort: 7203,
+        nodeServer: './src/app.js'
+    };
+
+    config.getWiredepDefaultOptions = function () {
+        var options = {
+            bowerJson: config.bower.json,
+            directory: config.bower.directory,
+            ignorePath: config.bower.ignorePath
+        };
+
+        return options;
+    }
+    
+    return config;
+}
